f5230fb82985617309ca504012e4a4045b2f2dd5
SeePeaJay <seercookie@gmail.com>
2021-09-21 01:15:54 -0700

    [WIP] Add code that scans engram into blocks and separators

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,9 @@
 const PATTERNS = {
-	rootBlockSeparator: /\n(?:\s|\t)*\n/
+	unorderedList: /^. (.|\n(?!\t*\n)(?!\t*$))*$/,
+	orderedList: /^\d{1,9}. (.|\n(?!\t*\n)(?!\t*$))*$/,
+
+	rootBlockSeparator: /\n(?:\s|\t)*\n/,
+	listItemSeparator: /(?!\n\n$)\n(?:\s|\t)*(?=(\d{1,9})?. (.|\n(?!\t*\n)(?!\t*$))*)/ // best attempt; you want a lookbehind with \n
 };
 
 module.exports = PATTERNS;
--- src/lexer.js
+++ src/lexer.js
@@ -8,12 +8,57 @@ class Lexer {
 		// this.ignoredPatterns = new Map();
     }
 
-	splitToBlocksAndBlockSeparators(engram) {
-		const rootBlocksAndRootBlockSeparators = engram.split(
-			new RegExp(`(${PATTERNS.rootBlockSeparator.source})`, 'g') // parentheses to include delimiter in result
-		);
-		this.blocksAndBlockSeparators.push(...rootBlocksAndRootBlockSeparators);
+	scan(engram) { // splitToBlocksAndBlockSeparators
+		const trimmedEngram = engram.trim();
+		const rootBlocks = trimmedEngram.split(PATTERNS.rootBlockSeparator); // is capturing group fine now?
+		this.blocksAndBlockSeparators.push(...this.getBlocksAndBlockSeparators(rootBlocks));
+		// splitListsToListItemsAndListItemSeparators
+		// this.blocksAndBlockSeparators = getBlocksAndBlockSeparators(rootBlocks);
+		// removeUnecessaryWhitespaceFromBlock(blocksAndSeparators)??
 	}
+
+	getBlocksAndBlockSeparators(rootBlocks) {
+		let blocksAndBlockSeparators = [...rootBlocks];
+
+		for(let i = 0; i < 2 * (rootBlocks.length - 1); i += 2) { // insert root block separators
+			blocksAndBlockSeparators.splice(i + 1, 0, '\n\n');
+		}
+
+		const listPattern = new RegExp(`(${PATTERNS.unorderedList.source})|${PATTERNS.orderedList.source}`, 'g');
+		for (let i = 0; i < blocksAndBlockSeparators.length; i += 2) {
+			if (blocksAndBlockSeparators[i].match(listPattern)) {
+				// console.log(blocksAndBlockSeparators[i]);
+				blocksAndBlockSeparators.splice(i, 1, ...this.getListItemsAndListItemSeparators(list));
+			}
+		}
+		
+		return blocksAndBlockSeparators;
+	}
+
+	getListItemsAndListItemSeparators(list) { // should be able to clean up SAfterListItemSeparatorAndBeforeList
+		// maxIndentLevel = 1;
+		// const listItemsAndListItemSeparators = list.split();
+		// for each list item separator (skip by 2 again)
+			// get number of \t
+			// if number of \t > maxIndentLevel
+				// trim
+				// maxIndentLevel ++;
+			// else if number of \t = maxIndentLevel
+				// maxIndentLevel ++;
+			// else \t < maxIndentLevel
+				// maxIndentLevel = number of \ts + 1
+		return [];
+	}
+
+	// scan(engram) {
+	//  remove unnecessary whitespace at the beginning and end; truncate engram
+	// 	rootBlocks -> maybe at this point get \n\n already, split then insert?
+	//  blocksAndBlockSeparators -> spread operator stuff
+	// 	remove whitespace from blocks
+	//	// for each blocksAndSeparators
+	// 	// remove tabs in evens and odds
+	// 	// for list items, check with counter
+	// }
 }
 
 module.exports = Lexer;
